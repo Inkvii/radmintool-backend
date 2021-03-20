1ba3243fb8cbace0bc66f9635f0e36c2dd48ad3c
Lubos Verzich <lve@paymorrow.com>
2021-03-20 11:35:07 +0100

    Added subscription monitoring

--- src/main/kotlin/com/example/radmintool/radmintool/TransactionWebsocketHandler.kt
+++ src/main/kotlin/com/example/radmintool/radmintool/TransactionWebsocketHandler.kt
@@ -1,20 +1,27 @@
 package com.example.radmintool.transaction
 
+import com.example.radmintool.radmintool.RadmintoolService
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.EventListener
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.messaging.support.GenericMessage
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.messaging.SessionSubscribeEvent
 
 /**
  * Websocket controller listens to requests and broadcasts it to all subscribers
  */
 @Controller
-class TransactionWebsocketController {
+class TransactionWebsocketController(
+    val radmintoolService: RadmintoolService
+) {
     private val log = LoggerFactory.getLogger(javaClass)
 
     @MessageMapping("/test")
@@ -22,6 +29,36 @@ class TransactionWebsocketController {
     fun testSendingMessage(message: String): String {
         log.info("Somebody is sending test message of " + message)
         return "Hello to you too!"
+    }
+
+    @MessageMapping("/money")
+    @SendTo("/topic/money")
+    fun requestMoneyCounterUpdate() {
+        log.info("Somebody is requesting update on money coutner")
+        radmintoolService.notifyThatNewTransactionWasCaptured()
+    }
+
+
+}
+
+@Component
+class WebsocketMonitoring {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+
+    @EventListener
+    fun handleSessionSubscribeEvent(event: SessionSubscribeEvent) {
+        val message = event.message as GenericMessage
+        val destination = message.headers.get("simpDestination")
+        val messageType = message.headers.get("simpMessageType")
+        val sessionId = message.headers.get("simpSessionId")
+        log.debug(
+            """
+            Message type: $messageType 
+            Destination: $destination
+            Session id: $sessionId
+        """.trimIndent()
+        )
     }
 }
 
